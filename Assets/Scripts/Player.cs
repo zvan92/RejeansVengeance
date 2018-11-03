00dcf4b38712d9d130efacbf700ff9cfd9aaa59c
Relag93 <rejean.lagasse93@gmail.com>
2018-11-03 13:35:27 -0400

    Changes to Playerscript

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
+    const float cHealthDrain = -3.0f;
+    const float totalHealth = 100;
+
     public float health;
     public float healthDrain;
     public int damage;
     private int timeForDrain;
+    [SerializeField]
+    private float speed;
 
+    public Image healthBar;
 	// Use this for initialization
 	void Start ()
     {
-        timeForDrain = 60;	
+        timeForDrain = 60;
+        health = totalHealth;
 	}
 	
-    private void TakeDamage(int amount)
+    public void TakeDamage(int amount)
     {
         int playerDamage = amount;
         health -= playerDamage;
+        healthBar.fillAmount = health / totalHealth;
     }
 
-    private void pickupCocaine()
+    public void pickupCocaine()
     {
         healthDrain = healthDrain = 2;
         damage = damage * 2;
@@ -29,25 +38,25 @@ public class Player : MonoBehaviour {
     IEnumerator cocaineTimer()
     {
        yield return new WaitForSeconds(5);
-        healthDrain = 1;
+        healthDrain = cHealthDrain * 2;
         damage = damage / 2;
     }
 
-    private void pickupBandaid()
+    public void pickupBandaid()
     {
-        healthDrain = 0;
+        healthDrain = cHealthDrain * 0;
         StartCoroutine("Bandaid");
     }
 
     IEnumerator bandaidTimer()
     {
         yield return new WaitForSeconds(5);
-        healthDrain = 1;
+        healthDrain = cHealthDrain;
     }
 
 	// Update is called once per frame
 	void Update ()
     {
-        health += healthDrain * Time.deltaTime;
+        health += cHealthDrain * Time.deltaTime;
 	}
 }
