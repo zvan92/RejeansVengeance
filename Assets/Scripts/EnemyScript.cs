d526de9e3f9eb0c714611a58e2f801145d883a11
MasonLee219  defintielymason@gmail.com <defintielymason@gmail.com>
2018-11-03 15:26:05 -0400

    Adding the Navmesh, Enemy, and some obstacles to world scene as well as some chagnes to EnemyScript

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,15 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour {
+public class EnemyScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int health;
+    public int range;
+    public float attackRate;
+    UnityEngine.AI.NavMeshAgent agent;
+    private Transform player;
+    private float timeLastAttacked;
+    private bool isDead;
+
+    // Use this for initialization
+    void Start()
+    {
+        isDead = false;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead)
+        {
+            return;
+        }
+
+        //Makes the robot face the player
+        transform.LookAt(player);
+
+        //Makes the robot go to the player, using the navmesh
+        agent.SetDestination(player.position);
+        /* if (Vector3.Distance(transform.position, player.position) < range
+       && Time.time - timeLastAttacked > attackRate)*/
+        //The enemy attacks the player when they enter the collider of Hitbox
+
+        {
+            //Calls the attack function and resets the timeLastAttacked
+            timeLastAttacked = Time.time;
+            attack();
+        }
+    }
+
+    private void attack()
+    {
+        Debug.Log("Fire");
+    }
 }
