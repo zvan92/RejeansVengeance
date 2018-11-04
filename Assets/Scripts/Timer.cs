5af96e0d324ea71597a25000cbc0de77dca75d5d
farceur2kaush <farceur.kaush@gmail.com>
2018-11-04 17:29:09 -0500

    UI wave  survived kinda works in world scene

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,15 +7,17 @@ public class Timer : MonoBehaviour {
 
     [SerializeField] private Text timerText;
     [SerializeField] private float wavetimer;
+    [SerializeField] private Text wavesSurvived;
 
     private float timer;
+    private int waveno = 0;
 
     void Start()
     {
         timer = wavetimer;
     }
 
-    void Update()
+    void timerF()
     {
         if (timer >= 0.0f)
         {
@@ -26,14 +28,33 @@ public class Timer : MonoBehaviour {
         else if (timer <= 0.0f)
         {
             timer = 0.0f;
-            timerText.text = "0.00";
-            GameOver();
+            timerText.text = timer.ToString("");
+            Wavesurvived();
         }
+
+    }
+
+    void Wavesurvived()
+    {
+        if (waveno < 1)
+        {
+            waveno++;
+        }
+        wavesSurvived.text = "Waves Survived: " + waveno;
+
+        GameObject gameOverParent = GameObject.Find("UI");
+        GameObject gameOver = gameOverParent.transform.Find("WavesSurvived").gameObject;
+        gameOver.SetActive(true);
+    }
+
+    void Update()
+    {
+        timerF();
     }
 
     void GameOver()
     {
-
+        
         //Load a new scene
     }
 }
